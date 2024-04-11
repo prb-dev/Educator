d69df46b4cbdb2cd339fe2ad616925242f4a7bb3
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-04-11 14:43:28 +0530

    update service name

--- schedule service/index.js
+++ schedule service/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import scheduleRouter from "./routes/schedule.route.js";
 
 dotenv.config();
 
@@ -17,4 +18,17 @@ const app = express();
 
 app.listen(3000, () => {
   console.log("learner service is running on port 3000");
+});
+
+app.use("/schedule", scheduleRouter);
+
+app.use((req, res, error, next) => {
+  const statusCode = error.statusCode || 500;
+  const errorMessage = error.message || "Internal server error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    errorMessage,
+  });
 });
