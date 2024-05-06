d2cbe5d7526e80ec199571659109788c2dea3de5
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-06 19:01:01 +0530

    create course service class

---
+++ courseService/services/course.service.js
@@ -0,0 +1,95 @@
+const Course = require("../model/Course");
+
+class CourseService {
+  async getAllCourses() {
+    const courses = await Course.find();
+    return courses;
+  }
+
+  async getCourseById(cid) {
+    const course = await Course.findById(cid);
+    return course;
+  }
+
+  async createCourse(data) {
+    const course = new Course(data);
+    const newCourse = await course.save();
+    return newCourse;
+  }
+
+  async updateCourse(cid, data) {
+    await Course.findByIdAndUpdate(cid, data);
+  }
+
+  async deleteCourse(cid) {
+    await Course.findByIdAndDelete(cid);
+  }
+
+  async answerQuiz(cid, data) {
+    const course = await Course.findById(cid);
+
+    // Retrieve questions and answers from request body
+    const { answers } = data;
+
+    if (!answers || !Array.isArray(answers)) {
+      return res.status(400).json({ message: "Invalid answers format" });
+    }
+
+    let score = 0;
+
+    // Loop through each answer and compare with correct answer
+    for (let i = 0; i < answers.length; i++) {
+      const questionIndex = answers[i].questionIndex;
+      const selectedAnswerIndex = answers[i].selectedAnswerIndex;
+
+      if (
+        course.questions[questionIndex] &&
+        course.questions[questionIndex].correctAnswerIndex ===
+          selectedAnswerIndex
+      ) {
+        score++;
+      }
+    }
+
+    return score;
+  }
+
+  async uploadLectureNotes(cid, file) {
+    const course = await Course.findById(cid);
+
+    // Upload file to Cloudinary
+    const result = await cloudinary.uploader.upload(file, {
+      resource_type: "raw",
+      folder: "lecture_notes", // You can customize the folder name
+      public_id: `${cid}_lecture_notes`, // Public ID of the file
+    });
+
+    // Save lecture notes URL to the course
+    course.lectureNotesUrl = result.secure_url;
+    await course.save();
+
+    return result;
+  }
+
+  async getCourses(cids) {
+    const courses = await Course.find({ _id: { $in: cids } });
+    return courses;
+  }
+
+  async eventHandler(payload) {
+    try {
+      switch (payload.event) {
+        case "GET_COURSES":
+          return this.getCourses(payload.cids);
+        case "GET_COURSE":
+          return this.getCourseById(payload.cid);
+        default:
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = CourseService;
