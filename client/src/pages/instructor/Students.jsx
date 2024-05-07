6c056e1b43fa319bec68485703f315f73a41c9ee
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-07 16:57:14 +0530

    add a modal and update action button

--- client/src/pages/instructor/Students.jsx
+++ client/src/pages/instructor/Students.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Box, IconButton } from "@mui/material";
+import { Backdrop, Box, Button, Fade, IconButton, Modal } from "@mui/material";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import ShowChartOutlinedIcon from "@mui/icons-material/ShowChartOutlined";
+import { Popover, Progress } from "antd";
+import { useLocation } from "react-router-dom";
 
 export default function Students() {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const cname = params.get("course");
+
+  const handleClose = () => setOpen(false);
+
+  const handleOpen = () => setOpen(true);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    borderRadius: 5,
+    boxShadow: 24,
+    p: 4,
+  };
+
   const data = [
     {
       id: 1,
@@ -93,21 +117,26 @@ export default function Students() {
       flex: 1,
     },
     {
-      field: "age",
-      headerName: "Age",
-      flex: 1,
-      headerAlign: "left",
-      align: "left",
-      type: "number",
-    },
-    {
       field: "access",
       headerName: "Action",
       renderCell: () => {
         return (
-          <IconButton>
-            <VisibilityOutlinedIcon />
-          </IconButton>
+          <Popover
+            trigger="click"
+            content={
+              <Button
+                variant="text"
+                onClick={handleOpen}
+                startIcon={<VisibilityOutlinedIcon />}
+              >
+                View Progess
+              </Button>
+            }
+          >
+            <IconButton>
+              <MoreVertOutlinedIcon />
+            </IconButton>
+          </Popover>
         );
       },
     },
@@ -118,6 +147,8 @@ export default function Students() {
       <h1 className="text-slate-700 text-2xl m-5 mb-6">
         Monitor learner progress <ShowChartOutlinedIcon />
       </h1>
+
+      <h2 className="text-slate-700 text-xl m-5 mb-6">{cname}</h2>
       <div className="cursor-pointer">
         <Box sx={{ height: "100%", width: "100%", overflow: "hidden" }}>
           <DataGrid
@@ -143,6 +174,42 @@ export default function Students() {
           />
         </Box>
       </div>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={() => handleClose()}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <h1 className="text-lg text-slate-700">Student Progess</h1>
+
+            <section className="flex justify-between mt-5">
+              <div className="grid place-items-center gap-5">
+                <p className="text-slate-500">Lecture progress </p>
+                <Progress type="circle" percent={50} />
+              </div>
+
+              <div className="grid place-items-center gap-5">
+                <p className="text-slate-500">Quiz progress </p>
+                <Progress type="circle" percent={100} />
+              </div>
+            </section>
+            <section className="mt-10">
+              <p className="text-slate-500">Overall progress </p>
+              <Progress type="line" percent={100} />
+            </section>
+          </Box>
+        </Fade>
+      </Modal>
     </main>
   );
 }
