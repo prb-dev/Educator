73815d833d1b5c179de273f2a9207c35d86b574a
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-07 21:57:47 +0530

    update roles

--- Usermanagement/models/User.js
+++ Usermanagement/models/User.js
@@ -1,26 +1,32 @@
- 
+const { default: mongoose } = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  courses: [{
-    course: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true
+  courses: [
+    {
+      course: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      completedLectureCount: {
+        type: Number,
+        default: 0,
+      },
+      completedQuizCount: {
+        type: Number,
+        default: 0,
+      },
     },
-    completedLectureCount: {
-      type: Number,
-      default: 0
-    },
-    completedQuizCount: {
-      type: Number,
-      default: 0
-    }
-  }],
+  ],
   password: { type: String, required: true },
   Email: { type: String, required: true, unique: true },
-  role: { type: String, enum: ['admin', 'user','teacher'], required: true }
+  role: {
+    type: String,
+    enum: ["admin", "student", "instructor"],
+    required: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
