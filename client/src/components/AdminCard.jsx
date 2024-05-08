96012cd985b7fc286f30b2da49eaae5776ee9350
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-08 10:46:42 +0530

    update heading color

--- client/src/components/AdminCard.jsx
+++ client/src/components/AdminCard.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function AdminCard({ title, icon, desc }) {
   return (
     <main
-      className="m-5 p-4 text-slate-700 pt-7 pb-7 rounded-lg flex justify-between w-60 h-fit items-center hover:brightness-105 cursor-pointer border-green-600 border bg-green-600/5"
+      className="m-5 p-4 text-slate-700 pt-7 pb-7 rounded-lg flex justify-between w-60 h-fit items-center hover:brightness-105 cursor-pointer border-green-600/10 border shadow-lg shadow-slate-200 bg-green-600/5"
     >
       <div>
         <div className="flex items-center gap-2 text-lg">
           {icon}
           <p>{desc}</p>
         </div>
-        <h1 className="text-green-600">
+        <h1 className="text-green-800">
           {title}
         </h1>
       </div>
