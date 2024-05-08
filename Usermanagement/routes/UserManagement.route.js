4cf9a7b320c6b0357f28969a26d84eeae60348d9
Pasan Mihisara <99629832+MihisaraJayasinghe@users.noreply.github.com>
2024-05-08 15:05:34 +0530

    Update UserManagement.route.js

--- Usermanagement/routes/UserManagement.route.js
+++ Usermanagement/routes/UserManagement.route.js
@@ -2,9 +2,10 @@
 const express = require('express');
 
 const router = express.Router();
-const { signUp, deleteUser, getAllUsers, logIn, protectedRoute } = require('../controllers/UserManegementcontroller');
+const { signUp, getCoursesOfUser, deleteUser, getAllUsers, logIn, protectedRoute } = require('../controllers/UserManegementcontroller');
 
 router.post('/signup', signUp);
+router.get('/getCoursesByID', getCoursesOfUser);
 router.post('/login', logIn);
 router.get('/getAllUsers', getAllUsers);
 router.delete('/deleteUser/:userId', deleteUser);
