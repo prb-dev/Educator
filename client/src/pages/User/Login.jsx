65597b6204071dd3a25dd4aa31fade89da6a2f10
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-09 23:30:16 +0530

    integrate redux

--- client/src/pages/User/Login.jsx
+++ client/src/pages/User/Login.jsx
@@ -1,36 +1,50 @@
-import React, { useState } from 'react';
-import axios from 'axios';
- 
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signinStart,
+  signinSuccess,
+  signinFail,
+} from "../../redux/user/userSlice.js";
 
 function Login() {
- 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  
-  const handleSubmit = async () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.user);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8004/api/UserManagement/login', {
+      dispatch(signinStart());
+
+      const response = await axios.post("http://localhost:80/user/login", {
         username,
-        password
+        password,
       });
 
-
       const token = response.data.token;
+      const user = response.data.user;
+
+      dispatch(
+        signinSuccess({
+          user,
+        })
+      );
 
       // Save token to local storage
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
 
-      console.log(response.data); 
+      console.log(response.data);
     } catch (error) {
-      console.error('Error Login:', error.message);
-    };
-  }
+      console.error("Error Login:", error.message);
+    }
+  };
 
   return (
-    <div className='h-[1000px] '>
+    <div className="h-[1000px] ">
       <h1 className="mt-40 text-4xl text-center">Login</h1>
       <form onSubmit={handleSubmit} className="mt-10 text-center">
-         
         <input
           type="text"
           placeholder="Username"
@@ -38,7 +52,7 @@ function Login() {
           onChange={(e) => setUsername(e.target.value)}
           className="py-2 px-4 w-1/4 bg-transparent border rounded-md mb-4"
         />
-        <br/>
+        <br />
         <input
           type="password"
           placeholder="Password"
@@ -47,7 +61,10 @@ function Login() {
           className="py-2 px-4   w-1/4 bg-transparent border  text-white rounded-md mb-4"
         />
         <br></br>
-        <button type="submit" className="py-2 px-4 bg-white text-black rounded-md">
+        <button
+          type="submit"
+          className="py-2 px-4 bg-white text-black rounded-md"
+        >
           Log In
         </button>
       </form>
