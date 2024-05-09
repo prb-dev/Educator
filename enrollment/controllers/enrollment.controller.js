39f3710ac3c47b44a1d44c1b7a641f409c95357e
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-09 22:43:57 +0530

    integrating enrollment, payment and email

--- enrollment/controllers/enrollment.controller.js
+++ enrollment/controllers/enrollment.controller.js
@@ -2,9 +2,26 @@ import EnrollmentService from "../services/enrollment.service.js";
 
 const service = new EnrollmentService();
 
+export const enrollmentConflictCheck = async (req, res, next) => {
+  try {
+    const user = await service.enrollmentConflictCheck(
+      req.params.uid,
+      req.params.cid
+    );
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const enroll = async (req, res, next) => {
   try {
-    const updateUser = await service.enroll(req.params.uid, req.params.cid);
+    const user = await service.enrollmentConflictCheck(
+      req.params.uid,
+      req.params.cid
+    );
+    const updateUser = await service.enroll(user, req.params.cid);
 
     res.status(200).json(updateUser);
   } catch (error) {
