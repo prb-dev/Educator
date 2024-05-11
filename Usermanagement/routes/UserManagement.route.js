df966eda4ebe4e152455af876892a15b74666fc7
Pasan Mihisara <99629832+MihisaraJayasinghe@users.noreply.github.com>
2024-05-11 12:23:06 +0530

    Student dashboard courses page

--- Usermanagement/routes/UserManagement.route.js
+++ Usermanagement/routes/UserManagement.route.js
@@ -9,14 +9,16 @@ const {
   logIn,
   protectedRoute,
   getStudentsByCid,
-    getCoursesOfUser
+    getCoursesOfUser,
+    getUserByID
 } = require("../controllers/UserManegementcontroller");
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
-router.get('/getCoursesByID', getCoursesOfUser);
+router.get('/getCoursesByID/', getCoursesOfUser);
 router.get("/getAllUsers", getAllUsers);
 router.get("/getAllstudents/:cid", getStudentsByCid);
+router.get("/getUserById/:userId", getUserByID);
 router.delete("/deleteUser/:userId", deleteUser);
 router.get("/protected", protectedRoute);
 
