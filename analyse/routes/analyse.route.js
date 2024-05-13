59659a05a53e64d0fccf1dd396c6bb7759cb7bbb
it21288630 <99311971+IT21288630@users.noreply.github.com>
2024-05-13 17:35:24 +0530

    create verifyToken funtion and import it

--- analyse/routes/analyse.route.js
+++ analyse/routes/analyse.route.js
@@ -3,10 +3,11 @@ import {
   getDashboardData,
   studentCourseProgress,
 } from "../controllers/analyse.controller.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/dashboard/:iid", getDashboardData);
-router.get("/:uid/:cid", studentCourseProgress);
+router.get("/dashboard/:iid", verifyToken, getDashboardData);
+router.get("/:uid/:cid", verifyToken, studentCourseProgress);
 
 export default router;
